162a6944f79b29acf99d73f604644cc369a2b934
Elisabeth Post <elisabeth.post@centauricorp.com>
2023-07-21 11:58:47 -0600

    setup ES6, add Prompt files and test files

---
+++ Create_Largest_Number/Create_Largest_Number.js
@@ -0,0 +1,5 @@
+//Write a function and corresponding tests that take 3 individual numbers and find the largest number that can be made from them
+
+
+
+
---
+++ Transpose/Transpose.js
@@ -0,0 +1,3 @@
+//Write a function called transpose that takes a 2D array of numbers and returns a new 2D array that is the transpose of the input array.
+//The transpose of a matrix is a new matrix that has the rows and columns of the original matrix swapped.
+//For example, if the input array is[[1, 2], [3, 4], [5, 6]], the output array should be[[1, 3, 5], [2, 4, 6]].
